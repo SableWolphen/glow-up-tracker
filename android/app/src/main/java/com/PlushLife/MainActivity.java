fca4b97ffefe974d7669df2c7c1e0b33c3320ac4
SableWolphen <309909614+SableWolphen@users.noreply.github.com>
2026-08-02 02:08:40 +0000

    Handle Android 15 edge-to-edge display properly

--- android/app/src/main/java/com/PlushLife/MainActivity.java
+++ android/app/src/main/java/com/PlushLife/MainActivity.java
@@ -1,11 +1,13 @@
 package com.PlushLife;
 
 import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
         registerPlugin(WidgetBridgePlugin.class);
         super.onCreate(savedInstanceState);
     }
